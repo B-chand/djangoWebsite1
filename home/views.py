6e0b4f232326c377c8f42f9d34c3c8b6b4004f5d
Bibek Chand <bibekchand015@gmail.com>
2026-02-07 09:40:26 +0545

    Update project

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,HttpResponse
 from datetime import datetime
 from home.models import Contact
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -27,6 +28,7 @@ def contact(request):
         desc=request.POST.get('desc')
         contact=Contact(name=name, email=email, phone=phone, desc=desc, date=datetime.today())
         contact.save()
+        messages.success(request, "Your message has been sent successfully!")
     return render(request, 'contact.html')
     # return HttpResponse("This is contact page of HelloD project.")
 
